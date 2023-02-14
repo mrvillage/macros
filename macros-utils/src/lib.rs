1621b6d7350a9626ab3c3076570de7b3b795398f
mrvillage <mr.village05@gmail.com>
2023-02-13 22:56:29 -0500

    Add MacroStream type

--- macros-utils/src/lib.rs
+++ macros-utils/src/lib.rs
@@ -1,3 +1,31 @@
-pub fn parse_arbitrary_ident() -> Result<String, String> {
-    Ok("".into())
+mod error;
+mod parsers;
+
+use std::collections::VecDeque;
+
+pub use error::{MacrosError, MacrosErrorKind};
+use proc_macro2::{TokenStream, TokenTree};
+
+#[derive(Debug)]
+struct MacroStream {
+    stream: VecDeque<TokenTree>,
+}
+
+impl MacroStream {
+    fn new(stream: TokenStream) -> Self {
+        let mut vec = VecDeque::new();
+        for i in stream.into_iter() {
+            vec.push_back(i);
+        }
+        Self { stream: vec }
+    }
+
+    fn next(&mut self) -> Option<TokenTree> {
+        let t = self.stream.pop_front();
+        t
+    }
+
+    fn peek(&self) -> Option<&TokenTree> {
+        self.stream.front()
+    }
 }
