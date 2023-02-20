f155bd677414a5a1fa5d15df63e6ef887a120dae
mrvillage <mr.village05@gmail.com>
2023-02-19 23:51:41 -0500

    Remove tests and add exports

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,20 +1,2 @@
 pub use macros_macros::parser;
-
-parser! {TestParser =>
-    {{}$ : help}@
-}
-
-#[cfg(test)]
-mod tests {
-    use std::str::FromStr;
-
-    use super::*;
-    use macros_utils::Parse;
-    use proc_macro2::TokenStream;
-
-    #[test]
-    fn it_works() {
-        let r = TestParser::parse(&mut TokenStream::from_str("chicken").unwrap().into()).unwrap();
-        panic!("PARSED {:?}", r);
-    }
-}
+pub use macros_utils::*;
